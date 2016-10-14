b13cd5710cc86a7f04d28853d7b6d5e0a164c404
Denis Korkhov <sqdron.io@gmail.com>
2016-10-14 15:42:14 +0300

    Message handler improvements

--- connect/transport.nats.go
+++ connect/transport.nats.go
@@ -1,26 +1,34 @@
 package connect
 
 import (
-	"github.com/nats-io/nats"
 	"encoding/json"
-	"time"
 	"fmt"
-	"reflect"
-	"log"
+	"github.com/nats-io/nats"
 	"github.com/sqdron/squad/util"
+	"log"
+	"time"
 )
 
 type transport struct {
 	connection *nats.Conn
 }
 
+func NatsTransport(url string) ITransport {
+	nc, e := nats.Connect(url)
+	if e != nil {
+		panic(e)
+	}
+	log.Println("Nats client started at: " + url)
+	return &transport{connection: nc}
+}
+
 func (t *transport) Subscribe(s string, cb interface{}) {
 	t.QueueSubscribe(s, "", cb)
 }
 
-func (t *transport) Publish(s string, message interface{}) error{
+func (t *transport) Publish(s string, message interface{}) error {
 	data, e := json.Marshal(message)
-	if (e != nil){
+	if e != nil {
 		return e
 	}
 	return t.connection.Publish(s, data)
@@ -37,39 +45,26 @@ func (t *transport) RequestSync(s string, message interface{}, timout time.Durat
 	data, _ := json.Marshal(message)
 	msg, e := t.connection.Request(s, data, timout)
 	fmt.Println(msg)
-	if (e != nil) {
+	if e != nil {
 		return nil, e
 	}
 	return msg.Data, nil
 }
 
-func NatsTransport(url string) ITransport {
-	nc, e := nats.Connect(url)
-	if e != nil {
-		panic(e)
-	}
-	log.Println("Nats client started at: " + url)
-
-	return &transport{connection: nc}
-}
-
 func (t *transport) QueueSubscribe(s string, group string, cb interface{}) {
 
-	dispatcher := &messageDispatcher{}
-	dispatcher.Use(UnmarshalMessageFor(cb))
-	dispatcher.Use(ApplyCallback(cb))
-
 	t.connection.QueueSubscribe(s, group, func(m *nats.Msg) {
-		dispatcher.Apply(m)
+		result, _ := dispatchMessage(s, m.Data, cb)
 		if m.Reply != "" {
-			var data []byte
-			switch responce.(type) {
-			default:
-				data, _ = json.Marshal(responce)
-			case []byte:
-				data = responce.([]byte)
+			var data []byte = []byte{}
+			if result != nil {
+				switch result.(type) {
+				default:
+					data, _ = json.Marshal(result)
+				case []byte:
+					data = result.([]byte)
+				}
 			}
-			fmt.Println(data)
 			t.connection.Publish(m.Reply, data)
 		}
 	})
